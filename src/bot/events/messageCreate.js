b1d3c97ce3c23a96d0473342ef827a922caa6cdf
Tanner <treynolds4@student.yosemite.edu>
2020-11-25 14:21:39 -0800

    Update messageCreate.js

--- src/bot/events/messageCreate.js
+++ src/bot/events/messageCreate.js
@@ -2,7 +2,7 @@ async function messageCreate(msg) {
     if (msg.content.indexOf(this.c.prefix) !== 0) return;
     const args = msg.content.slice(this.c.prefix.length).trim().split(/ +/g);
     const command = args.shift().toString().toLowerCase();
-    if (!this.c.discordAdminIDs.includes(msg.author.id) && command !== 'leaderboard') return;
+    if (!this.c.discordAdminIDs.includes(msg.author.id) && command !== 'leaderboard' && command !== 'help') return;
     for (let i = 0; this.commands.length > i; i++) {
         if (this.commands[i].command === command) {
             this.commands[i].execute(this, msg, args);
